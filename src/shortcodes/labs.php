659a0481483bd5c4b20784688df0b50ecaf8347c
Andy Pearson <andy@londonparkour.com>
2021-04-06 13:03:06 +0100

    remove styles

--- src/shortcodes/labs.php
+++ src/shortcodes/labs.php
@@ -4,9 +4,7 @@
  * Create the class and return results.
  */
 function andyp_labs_stack_callback($atts){
-    
-    wp_register_style( 'andyp_labs_css', ANDYP_LABS_STACK_PATH.'src/sass/style.css' );
-    wp_enqueue_style( 'andyp_labs_css' );
+
 
     $stack = new \andyp\labsstack\REST\labs();
 
